67afa4f6b6d411ac1973e4e08a1abc175cdb3941
piedrahitarodriguezanderson-code <piedrahitarodriguezanderson@gmail.com>
2025-08-08 15:45:27 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,11 @@
+<?php
+$Identificacion = $_POST['Identificacion'];
+$NomApellidos   = $_POST['NomApellidos'];
+$Cargo          = $_POST['Cargo'];
+$Salario        = $_POST['Salario'];
 
+echo "La identificación del Empleado es: " . $Identificacion . "<br>";
+echo "Los nombres y apellidos del empleado son: " . $NomApellidos . "<br>";
+echo "El cargo del empleado es: " . $Cargo . "<br>";
+echo "El salario del empleado es: " . $Salario;
+?>
